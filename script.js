4f37b5b6fec339a68df2159fc7f2bd8f63263312
design-120 <stride001design@gmail.com>
2025-07-18 20:09:57 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -18,18 +18,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
-
-  const swiper = new Swiper('.swiper', {
-    loop: true,
-    autoplay: {
-      delay: 3000,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
